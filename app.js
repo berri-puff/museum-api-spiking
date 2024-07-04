c0bb7c2ae1a1ab6c5f26b6c5277e5968f58cdbb2
berri-puff <jennyzhang0207@gmail.com>
2024-07-04 17:27:50 +0100

    va axios request for objects containing a keyword

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const axios = require("axios")
 
 // V&A gets a single record 
-axios.get("https://api.vam.ac.uk/v2/museumobject/O828146").then(function(result){
-const details = result.data["record"]
-console.log(details)
+// axios.get("https://api.vam.ac.uk/v2/museumobject/O828146").then(function(result){
+// const details = result.data["record"]
+// console.log(details)
+// })
+
+// V&A getting multiple object records with images and details of display and location for matching SOME WORDS whether that be place, name, location etc 
+axios.get('https://api.vam.ac.uk/v2/objects/search?q="England"').then(function(result){
+    console.log(result.data.records[0])
 })
 
-// V&A getting images
-axios.get()
+
+
